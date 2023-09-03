4cbdcf60a29a2d47daec5107f9cbcd93995937df
AnnaTopchii <113939011+AnnaTopchii@users.noreply.github.com>
2023-09-03 19:11:38 +0300

    ADD: animation on start page

--- src/components/Main/Hero.jsx
+++ src/components/Main/Hero.jsx
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from "react";
+
 import css from "./Hero.module.css";
 import mainMob from "../../images/main-mob.jpg";
 import mainTabNote from "../../images/main-tab-note.jpg";
@@ -5,9 +7,29 @@ import mainDesk from "../../images/main-desk.jpg";
 import seaViewMob from "../../images/sea-view-mob.jpg";
 import seaViewTab from "../../images/sea-view-tab.jpg";
 import seaViewNoteDesk from "../../images/sea-view-note-desk.jpg";
+import { IoChevronDownSharp } from "react-icons/io5";
 
 export default function Hero() {
+  const [animate, setAnimate] = useState(false);
   const screenWidth = window.innerWidth;
+
+  useEffect(() => {
+    // Function to handle scroll event
+    const handleScroll = () => {
+      if (window.scrollY > 10) {
+        setAnimate(true); // Trigger animation when scrolled down 100px
+        window.removeEventListener("scroll", handleScroll); // Remove event listener to prevent continuous triggering
+      }
+    };
+
+    // Attach the scroll event listener
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener on unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   let image1Src;
   let image2Src;
@@ -29,29 +51,49 @@ export default function Hero() {
   }
 
   return (
-    <section className={css.hero_block}>
-      <div className={css.hero_top}>
-        <div className={css.text_block_top}>
-          <span className={css.text_best}>Best</span>
-          <span className={css.text_place}>place You have</span>
+    <>
+      {/* Full-screen image */}
+      <div
+        className={`${css.overlay} ${animate ? css.hide : ""}`}
+        style={{
+          opacity: animate ? 0 : 1,
+          transform: animate ? "scale(0.99)" : "scale(1)",
+        }}
+      >
+        <h1 className={css.logo}>Best resort</h1>
+        <div className={css.scroll}>
+          <IoChevronDownSharp className={css.icon} />
+          <span>
+            scroll down <br /> to start the experience
+          </span>
         </div>
-        <img
-          className={css.img_hero_top}
-          src={image1Src}
-          alt={"sun beds near the pool"}
-        />
       </div>
 
-      <div className={css.hero_bottom}>
-        <img
-          className={css.img_hero_bottom}
-          src={image2Src}
-          alt={"vip lounge with sun beds with sea view"}
-        />
-        <span className={css.text_ever}>ever</span>
-      </div>
+      {/* Content that appears after scrolling */}
+      <section className={css.hero_block}>
+        <div className={css.hero_top}>
+          <div className={css.text_block_top}>
+            <span className={css.text_best}>Best</span>
+            <span className={css.text_place}>place You have</span>
+          </div>
+          <img
+            className={css.img_hero_top}
+            src={image1Src}
+            alt={"sun beds near the pool"}
+          />
+        </div>
 
-      <span className={css.text_been_before}>Been before</span>
-    </section>
+        <div className={css.hero_bottom}>
+          <img
+            className={css.img_hero_bottom}
+            src={image2Src}
+            alt={"vip lounge with sun beds with sea view"}
+          />
+          <span className={css.text_ever}>ever</span>
+        </div>
+
+        <span className={css.text_been_before}>Been before</span>
+      </section>
+    </>
   );
 }
