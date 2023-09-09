69b10e55be94ba3939f2741f089ac59aae9c9eb8
AnnaTopchii <113939011+AnnaTopchii@users.noreply.github.com>
2023-09-09 15:15:44 +0300

    ADD: wedding page

--- src/pages/WeddingsPage.jsx
+++ src/pages/WeddingsPage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 
 import css from "../styles/general.module.css";
 import Title from "../components/Title/Title";
-import Subtitle from "../components/Title/Subtitle";
+import Weddings from "../components/Weddings/Weddings";
 
 export default function WeddingsPage() {
   useEffect(() => {
@@ -12,7 +12,7 @@ export default function WeddingsPage() {
   return (
     <div className={css.container}>
       <Title>Weddings</Title>
-      <Subtitle>Sorry, this page is in development</Subtitle>
+      <Weddings />
     </div>
   );
 }
